08cbb38a7950b80833d4255ede564b60cea419f7
Niterbox <silva.johnny10@gmail.com>
2017-07-25 18:42:33 -0300

    Criando rede social

---
+++ login.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<head>
+	<meta charset="UTF-8">
+	<title>Love</title>
+</head>
+<body>
+	<div class="container">
+		<div class="login">
+			<form>
+				<input type="email" placeholder="Email">
+				<input type="password" placeholder="Senha">
+				<input type="submit" value="Entrar">
+
+			</form>
+		</div>
+	</div>
+</body>
+</html>
